952eb53ee116c7864f00bb1b2d38731592726165
Solomon Grundy <ngwtt@yahoo.com>
2024-08-19 20:26:10 -0600

    build list in postConstruct

--- src/main/java/com/learnspring/demo/rest/StudentRestController.java
+++ src/main/java/com/learnspring/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.learnspring.demo.rest;
 
 import com.learnspring.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,19 +13,28 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    //define "/students" endpoint (list of all students)
+    private List<Student> theStudents;
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-
-        List<Student> theStudents = new ArrayList<>();
+    // @PostConstruct is only called once, therefore...
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
 
 
         theStudents.add(new Student("Kip", "Trimmle"));
         theStudents.add(new Student("Steve", "Tipps"));
         theStudents.add(new Student("Trish", "Rishel"));
 
+    }
+
+    //define "/students" endpoint (list of all students)
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
         return theStudents;
     }
+
+    
 
 }
