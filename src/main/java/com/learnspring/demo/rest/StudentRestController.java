142c516f6f763bbfa92004caaa1a4ae17e3cdbfa
Solomon Grundy <ngwtt@yahoo.com>
2024-08-19 20:38:22 -0600

    couple of small changes

--- src/main/java/com/learnspring/demo/rest/StudentRestController.java
+++ src/main/java/com/learnspring/demo/rest/StudentRestController.java
@@ -3,6 +3,7 @@ package com.learnspring.demo.rest;
 import com.learnspring.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +36,9 @@ public class StudentRestController {
         return theStudents;
     }
 
-    
+    @GetMapping('/students/{studentId}')
+    public Student getStudent(@PathVariable int studentId) {
+        return theStudents.get(studentId);
+    }
 
 }
